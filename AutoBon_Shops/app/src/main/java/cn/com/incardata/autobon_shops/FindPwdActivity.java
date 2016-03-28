7db72541a4f3ca8b1af5e2dbae9aba297288bae1
shuihuo379 <qazhh258369@sina.com>
2016-03-28 15:32:09 +0800

    登录,注册,找回密码界面

---
+++ AutoBon_Shops/app/src/main/java/cn/com/incardata/autobon_shops/FindPwdActivity.java
@@ -0,0 +1,7 @@
+package cn.com.incardata.autobon_shops;
+
+/**
+ * Created by zhangming on 2016/3/28.
+ */
+public class FindPwdActivity {
+}
---
+++ AutoBon_Shops/app/src/main/java/cn/com/incardata/autobon_shops/RegisterActivity.java
@@ -0,0 +1,13 @@
+package cn.com.incardata.autobon_shops;
+
+import android.os.Bundle;
+
+/**
+ * Created by zhangming on 2016/3/28.
+ */
+public class RegisterActivity extends BaseActivity{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
